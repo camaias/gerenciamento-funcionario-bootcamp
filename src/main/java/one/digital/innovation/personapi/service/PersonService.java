27f3e94a532e6c6e4f5a64594a1f3a8a04fb1ba3
Caroline Maia <camaiasouza@gmail.com>
2021-09-02 22:05:22 -0300

    Created READEME.md file

--- src/main/java/one/digital/innovation/personapi/service/PersonService.java
+++ src/main/java/one/digital/innovation/personapi/service/PersonService.java
@@ -1,5 +1,6 @@
 package one.digital.innovation.personapi.service;
 
+import lombok.AllArgsConstructor;
 import one.digital.innovation.personapi.dto.MessageResponseDTO;
 import one.digital.innovation.personapi.dto.request.PersonDTO;
 import one.digital.innovation.personapi.entity.Person;
@@ -14,15 +15,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonService {
         private PersonRepository personRepository;
 
         private final PersonMapper personMapper = PersonMapper.INSTANCE;
-
-        @Autowired
-        public PersonService(PersonRepository personRepository) {
-        this.personRepository = personRepository;
-    }
 
     public MessageResponseDTO createPerson(@RequestBody PersonDTO personDTO) {
         Person personToSave = personMapper.toModel(personDTO);
